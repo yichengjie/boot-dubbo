4e36f85a069831470aeed4de6ba7c1a06d50ec33
yichengjie <626659321@qq.com>
2019-06-02 10:38:37 +0800

    删除无用配置

--- dubbo-provider/src/main/java/com/yicj/study/provider/MyApplication.java
+++ dubbo-provider/src/main/java/com/yicj/study/provider/MyApplication.java
@@ -1,10 +1,12 @@
 package com.yicj.study.provider;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@MapperScan("com.yicj.study.provider.mapper")
 @ImportResource(locations="classpath:spring/spring-dubbo.xml")
 public class MyApplication {
     public static void main(String[] args) {
